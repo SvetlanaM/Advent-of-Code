d75ba9892ef538fc814bd17456a3e16585770f0c
SvetlanaM <svetlana@kompilator.cz>
2021-12-10 17:07:25 +0100

    Add day10 part 2

--- day10/day10.py
+++ day10/day10.py
@@ -5,22 +5,33 @@ opening_chars = '([{<'
 closing_chars = ')]}>'
 sum_of_corrupted = 0
 mapping_dict = {'(' : [')', 3], '[' : [']', 57], '{' : ['}', 1197], '<' : ['>', 25137]}
+
 mapping_dict_sums = {')' : 3, ']' : 57, '}' : 1197, '>' :  25137}
 
+mapping_dict_part2 = {'(' : 1, '[' : 2, '{' : 3, '<' :  4}
+
+total_points = []
 
 for line in data:
-    print(line)
     empty_array = []
     for key, char in enumerate(line):
-        print(key, char, empty_array)
         if char in closing_chars:
             last_char = empty_array.pop(-1)   
             if char != mapping_dict[last_char][0]:
-                print(last_char, mapping_dict[last_char][0])
-                sum_of_corrupted += mapping_dict_sums[char]
+                empty_array = []
                 break
         else:
             empty_array.insert(len(empty_array), char)
+    
+    acc = 0
+    for score in empty_array[::-1]:
+        acc = acc * 5 + mapping_dict_part2[score]
+    
+    if len(empty_array) != 0:
+        total_points.append(acc)
 
-print(sum_of_corrupted)
+print(total_points)
+middle_key = int(len(total_points)/2)
+middle_score = sorted(total_points)[middle_key]
+print(middle_score)
                
