a1256cd72a9326f13fe6385827bc75c45003a867
SvetlanaM <svetlana@kompilator.cz>
2022-01-22 16:20:35 +0100

    While instead for loop

--- 2015/day01/day01.py
+++ 2015/day01/day01.py
@@ -1,12 +1,12 @@
 from collections import Counter
 
 with open("2015/day01/input.txt") as file:
-    data = file.read().splitlines()
+    data = file.read().splitlines()[0]
 
 def part1():
     array_by_items = []
 
-    for d in data[0]:
+    for d in data:
         array_by_items.append(d)
 
     print(Counter(array_by_items)['('] - Counter(array_by_items)[')'])
@@ -15,13 +15,20 @@ part1()
 
 
 def part2():
-    temp_dict = {'(' : 0, ')' : 0}
     result = 0
+    i = 0
+    while result != -1:
+        result =  result + 1 if data[i] == "(" else result-1
+        i += 1
+    return i
 
-    for i, d in enumerate(data[0]):
-        temp_dict[d] += 1
-        result = temp_dict['('] - temp_dict[')']
-        if result == -1:
-            return i+1
+
+    #solution with for loop
+    # temp_dict = {'(' : 0, ')' : 0}
+    # for i, d in enumerate(data):
+    #     temp_dict[d] += 1
+    #     result = temp_dict['('] - temp_dict[')']
+    #     if result == -1:
+    #         return i+1
 
 print(part2())
