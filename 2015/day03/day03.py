5e3b5853933cf9b67573db9750688c3d98d1c2f1
SvetlanaM <svetlana@kompilator.cz>
2022-01-24 23:09:18 +0100

    Ref

--- 2015/day03/day03.py
+++ 2015/day03/day03.py
@@ -1,26 +1,22 @@
 with open("2015/day03/input.txt") as file:
     data = file.read().splitlines()[0]
 
-
+moves = {'^' : 1, 'v' : -1, '>' : 1, '<': -1}
 def part1(data):
     coordinations = []
     x, y = 0, 0
     coordinations.append((x, y))
     for move in data:
-        if move == '^':
-            x += 1
-        if move == 'v':
-            x -= 1
-        if move == '>':
-            y += 1
-        if move == '<':
-            y -= 1
+        if move in ['^', 'v']:
+            x += moves[move]
+        else:
+            y += moves[move]
         coordinations.append((x, y))
     return len(set(coordinations)), coordinations
 
 
 def part2():
-    santa_instructions = data[::1]
+    santa_instructions = data[::2]
     robo_santa_instructions = data[1::2]
     _, santas = part1(santa_instructions)
     _, robos = part1(robo_santa_instructions)
