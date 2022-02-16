fc3df6b305821a210315dcb05e9840a3f396d99a
SvetlanaM <svetlana@kompilator.cz>
2022-02-16 13:16:21 +0100

    Optimalisation

--- 2015/day06/day06.py
+++ 2015/day06/day06.py
@@ -8,7 +8,7 @@ with open("2015/day06/input.txt") as file:
 def generate_new_set(min_x:int, max_x:int, min_y:int, max_y:int):
     return set(itertools.product(range(int(min_x), int(max_x)+1), range(int(min_y), int(max_y)+1)))
 
-def generate_new(min_x:int, max_x:int, min_y:int, max_y:int):
+def generate_new_list(min_x:int, max_x:int, min_y:int, max_y:int):
     return list(itertools.product(range(int(min_x), int(max_x)+1), range(int(min_y), int(max_y)+1)))
 
 pattern_for_instructions = '^(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)'
@@ -22,11 +22,9 @@ def part1() -> int:
         grouped_data = list(result.groups())
         state, min_x, max_x, min_y, max_y = grouped_data
         if state == 'turn on':
-            temp_set = generate_new_set(min_x, min_y, max_x, max_y)
-            result_set = set.union(result_set, temp_set)
+            result_set = set.union(result_set, generate_new_set(min_x, min_y, max_x, max_y))
         elif state == 'turn off':
-            temp_set = generate_new_set(min_x, min_y, max_x, max_y)
-            result_set = result_set - temp_set
+            result_set = result_set - generate_new_set(min_x, min_y, max_x, max_y)
         elif state == 'toggle':
             temp_set = generate_new_set(min_x, min_y, max_x, max_y)
             diff_set = temp_set.difference(result_set)
@@ -38,27 +36,21 @@ def part1() -> int:
 
 def part2() -> int:
     result_list = []
-    removed_list = []
-    
+
     for line in data:
         result = re.match(pattern_for_instructions, line)
         grouped_data = list(result.groups())
         state, min_x, max_x, min_y, max_y = grouped_data
         if state == 'turn on':
-            temp_list = generate_new(min_x, min_y, max_x, max_y)
-            result_list += temp_list
+            result_list += generate_new_list(min_x, min_y, max_x, max_y)
         elif state == 'turn off':
-            temp_list = generate_new(min_x, min_y, max_x, max_y)
-            c = list((Counter(result_list) & Counter(temp_list)).elements())
-            removed_list += c
+            list_difference = Counter(result_list) - Counter(generate_new_list(min_x, min_y, max_x, max_y))
+            result_list = list(list_difference.elements())
         elif state == 'toggle':
-            temp_list = generate_new(min_x, min_y, max_x, max_y)
-            temp_list = generate_new(min_x, min_y, max_x, max_y)
-            result_list += temp_list
-            result_list += temp_list
-    
-    c = list((Counter(result_list) & Counter(removed_list)).elements())
-    return len(result_list) - len(c)
+            for _ in range(0, 2):
+                result_list += generate_new_list(min_x, min_y, max_x, max_y)
+
+    return len(result_list)
     
 
 
