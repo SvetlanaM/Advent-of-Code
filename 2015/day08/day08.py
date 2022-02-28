73e0beb41d19b5a03acb8d39f3e248fd908429c5
SvetlanaM <svetlana@kompilator.cz>
2022-02-28 11:16:53 +0100

    Update function call

--- 2015/day08/day08.py
+++ 2015/day08/day08.py
@@ -1,3 +1,6 @@
+from pyclbr import Function
+
+
 with open("2015/day08/input.txt") as file:
     data = file.read().splitlines()
 
@@ -8,19 +11,19 @@ def get_string_len(string: str) -> int:
     return len(eval(string))
 
 def get_new_string_len(string:str) -> int:
-    return len(r"{}".format(string)) + r"{}".format(string).count('"') + 2 + r"{}".format(string).count('\\')
+    return get_raw_string_len(string) + r"{}".format(string).count('"') + 2 + r"{}".format(string).count('\\')
+
+def day08(fn1:Function, fn2:Function) -> int:
+    total = 0
+    for line in data:
+        total += fn1(line) - fn2(line)
+    return total
 
 def part1() -> int:
-    total = 0
-    for line in data:
-        total += get_raw_string_len(line) - get_string_len(line)
-    return total
+    return day08(fn1 = get_raw_string_len, fn2 = get_string_len)
 
 def part2() -> int:
-    total = 0
-    for line in data:
-        total += get_new_string_len(line) - get_raw_string_len(line)
-    return total
+    return day08(fn1 = get_new_string_len, fn2 = get_raw_string_len)
 
 print("Part 1:", part1())
 print("Part 2:", part2())
