18d16f7d1820f4e291dcb81b574dee1ac3ea24f0
SvetlanaM <svetlana@kompilator.cz>
2022-03-26 17:57:54 +0100

    Map instead of for loop

--- 2015/day10/day10.py
+++ 2015/day10/day10.py
@@ -3,12 +3,12 @@ import re
 pattern_for_instructions = r'(\d)\1*'
 pattern_object = re.compile(pattern_for_instructions)
 
+
 def compute(num_of_repeat:int, exp_number:str = '1113122113') -> int:
   for _ in range(num_of_repeat):
     matched_group = pattern_object.finditer(exp_number)
     exp_number = ''
-    for m in matched_group:
-      exp_number += str(len(m.group())) + m.groups()[0]
+    exp_number = ''.join(map(lambda m: exp_number + str(len(m.group())) + m.groups()[0], matched_group))
   return len(exp_number)
   
 print("Part 1:", compute(40))
