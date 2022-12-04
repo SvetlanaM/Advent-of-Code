5e73635537eb8c10c7c2c5c65afcc0c8bb327ec6
SvetlanaM <svetlana@kompilator.cz>
2022-12-04 11:32:52 +0100

    Add tests to day04

--- 2022/day04/day04.py
+++ 2022/day04/day04.py
@@ -19,32 +19,25 @@ def convert_dataset(section: list[str]) -> tuple[list[int], list[int]]:
         
 
 # today, no one liners
-def get_sum_of_pairs(sections: list[list[str]]) -> int:
+def get_sum_of_pairs(sections: list[list[str]]) -> tuple[int, int]:
     sum_of_overlap = 0
-
+    sum_of_unique = 0
+    
     for section in sections:
         s1_list, s2_list = convert_dataset(section)
+        
         if set(s1_list).issubset(s2_list) or set(s2_list).issubset(s1_list):
             sum_of_overlap += 1
-    
-    return sum_of_overlap
-
-
-def get_single_overlap(sections: list[list[str]]) -> int:
-    sum_of_overlap = 0
-    
-    for section in sections:
-        s1_list, s2_list = convert_dataset(section)
+        
         if len(set(s1_list).intersection(s2_list)) > 0:
-            sum_of_overlap += 1
-            
-    return sum_of_overlap
+            sum_of_unique += 1
+    
+    return sum_of_overlap, sum_of_unique
     
 
 def main(file_path: str) -> tuple[int, int]:
     input_data = parse_data(file_path)
-    sum_of_pairs = get_sum_of_pairs(input_data)
-    sum_of_single = get_single_overlap((input_data))
+    sum_of_pairs, sum_of_single = get_sum_of_pairs(input_data)
     return sum_of_pairs, sum_of_single
 
 
@@ -52,3 +45,9 @@ if __name__ == "__main__":
     part_1, part_2 = main(INPUT_FILE.replace("test_", ""))
     print(f"Part 1: {part_1}")
     print(f"Part 2: {part_2}")
+
+   
+# tests 
+assert convert_dataset(['2-4', '6-8']) == ([2, 3, 4], [6, 7, 8])
+assert get_sum_of_pairs(parse_data(INPUT_FILE))[0] == 2
+assert get_sum_of_pairs(parse_data(INPUT_FILE))[1] == 4
