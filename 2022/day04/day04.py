d0e4185450539e4fd2ffc341b0696a1d34b53461
SvetlanaM <svetlana@kompilator.cz>
2022-12-04 19:25:40 +0100

    refactoring

--- 2022/day04/day04.py
+++ 2022/day04/day04.py
@@ -8,11 +8,11 @@ def parse_data(file_path: str) -> list[str]:
         return [line.split(',') for line in data]
 
 
-def convert_dataset(section: list[str]) -> tuple[list[int], list[int]]:
+def convert_dataset(section: list[str]) -> tuple[set[int], set[int]]:
     section1, section2 = re.findall('\d+', section[0]), re.findall('\d+', section[1])
-    s1_list = list(range(int(section1[0]), int(section1[1]) + 1))
-    s2_list = list(range(int(section2[0]), int(section2[1]) + 1))
-    return s1_list, s2_list
+    s1 = set(range(int(section1[0]), int(section1[1]) + 1))
+    s2 = set(range(int(section2[0]), int(section2[1]) + 1))
+    return s1, s2
         
 
 # today, no one liners
@@ -21,12 +21,12 @@ def get_sum_of_pairs(sections: list[list[str]]) -> tuple[int, int]:
     sum_of_unique = 0
     
     for section in sections:
-        s1_list, s2_list = convert_dataset(section)
+        s1, s2 = convert_dataset(section)
         
-        if set(s1_list).issubset(s2_list) or set(s2_list).issubset(s1_list):
+        if s1.issubset(s2) or s2.issubset(s1):
             sum_of_overlap += 1
         
-        if len(set(s1_list).intersection(s2_list)) > 0:
+        if len(s1.intersection(s2)) > 0:
             sum_of_unique += 1
     
     return sum_of_overlap, sum_of_unique
@@ -45,6 +45,6 @@ if __name__ == "__main__":
 
    
 # tests 
-assert convert_dataset(['2-4', '6-8']) == ([2, 3, 4], [6, 7, 8])
+assert convert_dataset(['2-4', '6-8']) == ({2, 3, 4}, {6, 7, 8})
 assert get_sum_of_pairs(parse_data(INPUT_FILE))[0] == 2
 assert get_sum_of_pairs(parse_data(INPUT_FILE))[1] == 4
