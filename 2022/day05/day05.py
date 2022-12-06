97a8433058c38b955e2fd8236ce7424b2979104c
SvetlanaM <svetlana@kompilator.cz>
2022-12-06 12:58:57 +0100

    Super easy

--- 2022/day05/day05.py
+++ 2022/day05/day05.py
@@ -1,11 +1,11 @@
 import re
 
 INPUT_FILE = '2022/day05/test_input.txt'
-
 pattern_for_instructions = '^move (\d+) from (\d+) to (\d+)'
 
 with open(INPUT_FILE) as file:
     data = file.read().splitlines()
+    
     
 index_of_divider = data.index("") 
 num_of_deques = data[:index_of_divider][-1][-2]
@@ -14,8 +14,10 @@ moves = data[index_of_divider:len(data)][1:]
 moves_stack = []
 stack = []
 
+
 for move in moves:
     moves_stack.append(re.match(pattern_for_instructions, move).groups())
+
 
 for _ in range(int(num_of_deques)):
     stack.append(list())
@@ -34,9 +36,11 @@ for m in moves_stack:
     stack[int(to)-1].reverse()
     del stack[int(from_where)-1][0:int(move)]
 
+
 final_letters = []
 for item in stack:
     final_letters.append(item[0])
+
 
 print(','.join(final_letters).replace(",", ""))
 
@@ -51,5 +55,6 @@ for m in moves_stack:
 final_letters = []
 for item in stack:
     final_letters.append(item[0])
+
 
 print(','.join(final_letters).replace(",", ""))
---
+++ 2022/day06/day06.py
@@ -0,0 +1,46 @@
+import collections
+from itertools import islice
+from typing import Iterable
+
+INPUT_FILE = '2022/day06/test_input.txt'
+
+
+def parse_data(file_path: str) -> list[str]:
+    with open(file_path, encoding='utf-8') as file:
+        return file.read()
+
+# function from here - https://docs.python.org/3/library/itertools.html    
+def sliding_window(iterable: Iterable, n: int) -> Iterable:
+    # sliding_window('ABCDEFG', 4) --> ABCD BCDE CDEF DEFG
+    it = iter(iterable)
+    window = collections.deque(islice(it, n), maxlen=n)
+    if len(window) == n:
+        yield tuple(window)
+    for x in it:
+        window.append(x)
+        yield tuple(window)
+        
+
+def compute(data: list[str], position: int = 4) -> int:
+    for index, window in enumerate(sliding_window(data, position)):
+        if len(set(window)) == position:
+            return index + position
+
+# tests 
+assert compute('bvwbjplbgvbhsrlpgdmjqwftvncz') == 5
+assert compute('nppdvjthqldpwncqszvftbrmjlhg') == 6
+assert compute('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw') == 11  
+assert compute('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 14) == 26 
+
+
+def main(file_path: str) -> None:
+    input_data = parse_data(file_path)
+    num_of_chars = compute(input_data)
+    num_of_chars_2 = compute(input_data, 14)
+    print("Part 1:", num_of_chars)
+    print("Part 2:", num_of_chars_2)
+    
+    
+if __name__ == "__main__":
+    raise SystemExit(main(INPUT_FILE.replace("test_", "")))
+    
